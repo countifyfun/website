d42732a38a48fd753aecf3104ef58592f0cbe934
Not TCA <50563138+ToastedDev@users.noreply.github.com>
2023-12-30 16:58:26 +0800

    feat(redirects): add invite redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  redirects: async () => [
+    {
+      source: "/invite",
+      destination:
+        "https://discord.com/api/oauth2/authorize?client_id=1190299944062570627&permissions=26640&scope=bot%20applications.commands",
+      permanent: false,
+    },
+  ],
   images: {
     remotePatterns: [
       {
